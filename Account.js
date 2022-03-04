415afafba7fff502475d24f4cec7582e060a939b
Mdbshero <mdbshero@gmail.com>
2022-03-03 20:23:21 -0600

    Imported Accout.js to atm.js and added pin validation.

--- Account.js
+++ Account.js
@@ -2,3 +2,9 @@
 //user account includes current balance and pin
 const prompt = require('prompt-sync')();
 
+let user = {
+    pin:1234,
+    balance:0
+};
+module.exports.pin = user.pin;
+module.exports.balance = user.balance;
--- atm.js
+++ atm.js
@@ -1,1 +1,24 @@
 "use strict"
+const prompt = require('prompt-sync')();
+const account = require('./Account')
+//Validate Pin
+function valPin (userPin){
+    userPin = prompt('Please enter your four digit pin.');
+    if ((userPin.length) == 4){
+        if (userPin == account.pin){
+            return true;
+        } else {
+            console.log('This is the incorrect user pin, please try again.');
+            valPin();
+        }
+    } else {
+        console.log('User input was not a pin, please try again.');
+        valPin();
+    }
+}
+valPin()
+// get balance
+
+//Withdrawal
+
+//Deposit
